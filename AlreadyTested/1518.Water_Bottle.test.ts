fd72799c88b56a40e0fc7138ba7a9a920f2906a2
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-23 21:28:00 +0900

    test

---
+++ AlreadyTested/1518.Water_Bottle.test.ts
@@ -0,0 +1,18 @@
+import { numWaterBottles } from '../Algorithms/1518.Water_Bottles';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('Water Bottles', () => {
+	it('input: 9, 3; output: 13', () => {
+		expect(numWaterBottles(9, 3)).to.equal(13);
+	});
+	it('input: 15, 4; output: 19', () => {
+		expect(numWaterBottles(15, 4)).to.equal(19);
+	});
+	it('input: 5, 5; output: 6', () => {
+		expect(numWaterBottles(5, 5)).to.equal(6);
+	});
+	it('input: 2, 3; output: 2', () => {
+		expect(numWaterBottles(2, 3)).to.equal(2);
+	});
+});
