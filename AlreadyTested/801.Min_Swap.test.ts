cc4d8f703faa55864a7b6d915c1e5bf3ed4a2409
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-26 20:26:13 +0900

    tests

---
+++ AlreadyTested/801.Min_Swap.test.ts
@@ -0,0 +1,26 @@
+import { minSwap } from '../Algorithms/801.Min_Swap';
+import { describe } from 'mocha';
+import { expect } from 'chai';
+
+describe("Minimum Swap for creating strictly increasing Num Arr's", () => {
+	it('input: [1,3,5,4], [1,2,3,7]; output: 1', () => {
+		expect(minSwap([1, 3, 5, 4], [1, 2, 3, 7])).to.equal(1);
+	});
+	it('input: [0,3,5,8,9], [2,1,4,6,9]; output: 1', () => {
+		expect(minSwap([0, 3, 5, 8, 9], [2, 1, 4, 6, 9])).to.equal(1);
+	});
+	it('input: [0,4,4,5,9],  [0,1,6,8,10]; output: 1', () => {
+		expect(minSwap([0, 4, 4, 5, 9], [0, 1, 6, 8, 10])).to.equal(1);
+	});
+	it('input: [3,3,8,9,10],  [1,7,4,6,8]; output: 1', () => {
+		expect(minSwap([3, 3, 8, 9, 10], [1, 7, 4, 6, 8])).to.equal(1);
+	});
+	it('input: [0,7,8,10,10,11,12,13,19,18], [4,4,5,7,11,14,15,16,17,20]; output: 4', () => {
+		expect(
+			minSwap(
+				[0, 7, 8, 10, 10, 11, 12, 13, 19, 18],
+				[4, 4, 5, 7, 11, 14, 15, 16, 17, 20],
+			),
+		).to.equal(4);
+	});
+});
