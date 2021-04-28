a2f789689eb3206479ff1f091fa53d71285f0ee4
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-28 16:41:48 +0900

    test

---
+++ AlreadyTested/409.LP.test.ts
@@ -0,0 +1,28 @@
+import { longestPalindrome } from '../DataStructure/409.LongestPalindrome';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('Longest Palindrome', () => {
+	it("s = 'acbcccdd'; output: 7", () => {
+		expect(longestPalindrome('acbcccdd')).to.equal(7);
+	});
+	it("s = 'a'; output: 1", () => {
+		expect(longestPalindrome('a')).to.equal(1);
+	});
+	it("s = 'bb'; output: 2", () => {
+		expect(longestPalindrome('bb')).to.equal(2);
+	});
+	it("s = 'ccc'; output: 3", () => {
+		expect(longestPalindrome('ccc')).to.equal(3);
+	});
+	it("s ='ababababa'; output: 9", () => {
+		expect(longestPalindrome('ababababa')).to.equal(9);
+	});
+	it("s = 'civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'; output: 983", () => {
+		expect(
+			longestPalindrome(
+				'civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth',
+			),
+		).to.equal(983);
+	});
+});
