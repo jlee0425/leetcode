095bfc94ea791a1638a624fc119d40b7ed3eff84
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-04-28 18:25:05 +0900

    tested

---
+++ Algorithms/33.Search_In_RSA.test.ts
@@ -0,0 +1,23 @@
+import { search } from './33.Search_In_Rotated_Sorted_Arr';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('Search in Rotated Sorted Array', () => {
+	it('input: [1], 0; output: -1', () => {
+		expect(search([1], 0)).to.equal(-1);
+	});
+	it('input: [1,3], 3; output: 1', () => {
+		expect(search([1, 3], 3)).to.equal(1);
+	});
+	it('input: [4,5,6,7,0,1,2], 0; output: 4', () => {
+		expect(search([4, 5, 6, 7, 0, 1, 2], 0)).to.equal(4);
+	});
+
+	it('input: [4,5,6,7,0,1,2], 3; output: -1', () => {
+		expect(search([4, 5, 6, 7, 0, 1, 2], 3)).to.equal(-1);
+	});
+
+	it('input: [1,2,3,4,-4,-3,-2], 0; output: -3', () => {
+		expect(search([1, 2, 3, 4, -4, -3, -2], -3)).to.equal(5);
+	});
+});
