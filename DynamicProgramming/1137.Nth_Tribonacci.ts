188dcf6fd37c86f2fc37ac7d88dd0df8870a8729
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-04 17:23:57 +0900

    added desc

--- DynamicProgramming/1137.Nth_Tribonacci.ts
+++ DynamicProgramming/1137.Nth_Tribonacci.ts
@@ -1,3 +1,12 @@
+/**
+ * The Tribonacci sequence Tn is defined as follows:
+ * T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+ * Given n, return the value of Tn.
+ *
+ * @param n number
+ * @returns number; value of Tn
+ */
+
 export const tribonacci = (n: number): number => {
 	let dp: number[] = new Array(n + 1);
 	[dp[0], dp[1], dp[2]] = [0, 1, 1];
