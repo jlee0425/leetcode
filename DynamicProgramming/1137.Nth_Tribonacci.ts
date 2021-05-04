05d17075de3b6407282f5a8d05351253ff05cc92
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-04 17:15:28 +0900

    76ms & 38.7mb

--- DynamicProgramming/1137.Nth_Tribonacci.ts
+++ DynamicProgramming/1137.Nth_Tribonacci.ts
@@ -1,6 +1,7 @@
 export const tribonacci = (n: number): number => {
 	let dp: number[] = new Array(n + 1);
 	[dp[0], dp[1], dp[2]] = [0, 1, 1];
+
 	if (n < 3) return dp[n];
 
 	for (let i = 3; i < n + 1; i++) {
