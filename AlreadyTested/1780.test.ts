bf510ad27d1c50638fd71c6109a6d5f4b95a8cff
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-06 15:49:59 +0900

    test

---
+++ AlreadyTested/1780.test.ts
@@ -0,0 +1,17 @@
+import { checkPowersOfThree } from '../Algorithms/1780.Powers_of_Three';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('Check if Number is a Sum of Powers of Three', () => {
+	it('n = 12', () => {
+		expect(checkPowersOfThree(12)).to.equal(true);
+	});
+
+	it('n = 91', () => {
+		expect(checkPowersOfThree(91)).to.equal(true);
+	});
+
+	it('n = 21', () => {
+		expect(checkPowersOfThree(21)).to.equal(false);
+	});
+});
