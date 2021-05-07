8867b00d3a153160bce6ef6eeae1fc4dff06a702
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-07 16:47:39 +0900

    84ms & 40.4mb

---
+++ DataStructure/191.Number_of_1_Bits.ts
@@ -0,0 +1,26 @@
+/**
+ * Write a function that takes an unsigned integer and
+ * returns the number of '1' bits it has (also known as the Hamming weight).
+ *
+ * Note:
+ * Note that in some languages, such as Java, there is no unsigned integer type.
+ * In this case, the input will be given as a signed integer type.
+ * It should not affect your implementation, as the integer's internal binary representation is the same,
+ * whether it is signed or unsigned.
+ * In Java, the compiler represents the signed integers using 2's complement notation.
+ * Therefore, in Example 3, the input represents the signed integer. -3.
+ *
+ * @param n number
+ * @returns number
+ */
+
+export const hammingWeight = (n: number): number => {
+	// using bit manipulation
+	let int = BigInt(n);
+	let count = 0;
+	while (int) {
+		int & 1n && count++;
+		int >>= 1n;
+	}
+	return count;
+};
