8565d562bfe9289a4e2e20626f51ce825461dd58
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-05-11 18:07:41 +0900

    80ms & 39.1mb

---
+++ Algorithms/17.Letter_Comb.ts
@@ -0,0 +1,40 @@
+/**
+ * Given a string containing digits from 2-9 inclusive,
+ * return all possible letter combinations that the number could represent.
+ * Return the answer in any order.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given below.
+ * Note that 1 does not map to any letters.
+ *
+ * @param digits string
+ * @returns string[]
+ */
+
+export const letterCombinations = (digits: string): string[] => {
+	if (!digits) return [];
+
+	const letterDic = {
+		'2': ['a', 'b', 'c'],
+		'3': ['d', 'e', 'f'],
+		'4': ['g', 'h', 'i'],
+		'5': ['j', 'k', 'l'],
+		'6': ['m', 'n', 'o'],
+		'7': ['p', 'q', 'r', 's'],
+		'8': ['t', 'u', 'v'],
+		'9': ['w', 'x', 'y', 'z'],
+	};
+
+	if (digits.length === 1) {
+		return letterDic[digits];
+	}
+
+	let combinations: string[] = [];
+	let curr = letterCombinations(digits.substr(1));
+	for (let i = 0; i < letterDic[digits[0]].length; i++) {
+		for (let j = 0; j < curr.length; j++) {
+			combinations.push(letterDic[digits[0]][i] + curr[j]);
+		}
+	}
+
+	return combinations;
+};
