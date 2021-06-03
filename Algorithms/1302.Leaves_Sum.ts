1d46645fe45ab1954e3dfa29c471ef020c8ac3f6
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-04 05:56:22 +0900

    108ms & 49.2mb

---
+++ Algorithms/1302.Leaves_Sum.ts
@@ -0,0 +1,31 @@
+import { TreeNode } from './../DataStructure/1008.BST_Traversal';
+
+/**
+ * Given the root of a binary tree, return the sum of values of its deepest leaves.
+ *
+ * @param root TreeNode
+ * @returns number
+ */
+export const deepestLeavesSum = (root: TreeNode | null): number => {
+	if (!root) return 0;
+	let sum = 0;
+	let maxHeight = 0;
+
+	const dfs = (node: TreeNode, height: number = 0) => {
+		if (!node) return;
+
+		dfs(node.left, height + 1);
+		if (!node.left && !node.right) {
+			if (height > maxHeight) sum = 0;
+			maxHeight = Math.max(maxHeight, height);
+			if (height === maxHeight) {
+				sum += node.val;
+			}
+		}
+		dfs(node.right, height + 1);
+	};
+
+	dfs(root);
+
+	return sum;
+};
