951623fe02991736c9bdfe0e3fc442f49c2bfa5b
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-04 15:00:00 +0900

    352ms & 56.4mb

---
+++ AlreadyTested/1877.test.ts
@@ -0,0 +1,13 @@
+import { minPairSum } from './../Algorithms/1877.Min_Max_Sum';
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+describe('Minimize Maximum Pair Sum in Array', () => {
+	it('nums = [3,5,2,3]', () => {
+		expect(minPairSum([3, 5, 2, 3])).to.equal(7);
+	});
+
+	it('nums = [3,5,4,2,4,6]', () => {
+		expect(minPairSum([3, 5, 4, 2, 4, 6])).to.equal(8);
+	});
+});
