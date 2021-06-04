eaffbd66477d5dfb471d9fc50fcd31bd69974558
Jake Lee <40153724+jlee0425@users.noreply.github.com>
2021-06-05 00:05:50 +0900

    92ms & 38.9mb

--- DataStructure/1880.Sum_2_Words.ts
+++ DataStructure/1880.Sum_2_Words.ts
@@ -1,4 +1,18 @@
 /**
+ * The letter value of a letter is its position in the alphabet starting from 0
+ * (i.e. 'a' -> 0, 'b' -> 1, 'c' -> 2, etc.).
+ *
+ * The numerical value of some string of lowercase English letters s
+ * is the concatenation of the letter values of each letter in s, which is then converted into an integer.
+ *
+ * For example, if s = "acb", we concatenate each letter's letter value, resulting in "021".
+ * After converting it, we get 21.
+ *
+ * You are given three strings firstWord, secondWord, and targetWord,
+ * each consisting of lowercase English letters 'a' through 'j' inclusive.
+ *
+ * Return true if the summation of the numerical values of firstWord and secondWord
+ * equals the numerical value of targetWord, or false otherwise.
  *
  * @param firstWord string
  * @param secondWord string
@@ -9,23 +23,10 @@ export const isSumEqual = (
 	secondWord: string,
 	targetWord: string,
 ): boolean => {
-	const map = {
-		a: 0,
-		b: 1,
-		c: 2,
-		d: 3,
-		e: 4,
-		f: 5,
-		g: 6,
-		h: 7,
-		i: 8,
-		j: 9,
-	};
-
 	const getNumOfWord = (str: string): number => {
 		let num = 0;
 		for (let c of str) {
-			num = num * 10 + map[c];
+			num = num * 10 + (c.charCodeAt(0) - 'a'.charCodeAt(0));
 		}
 		return num;
 	};
